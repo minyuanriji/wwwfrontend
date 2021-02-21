5a42ac8e00236c4d13c276d6190d675654b009b7
apple <apple@appledeMacBook-Pro.local>
2021-02-21 16:09:43 +0800

    [!]修改购物券区无法显示按钮，修改运费bug

---
+++ pages/webview/webview.vue
@@ -0,0 +1,26 @@
+<template>
+	<view>
+		<web-view :src="url"></web-view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				url: '',
+				}
+		},
+		onLoad(option) {
+			var url = decodeURIComponent(option.url);
+			this.url = url.replace('http','https');
+		},
+		methods: {
+			
+		}
+	}
+</script>
+
+<style>
+
+</style>
