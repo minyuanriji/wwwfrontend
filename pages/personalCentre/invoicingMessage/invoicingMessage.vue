4e18597f7ac7e1e01f2fd4cd8ce6ffeac996e61c
z-xing-1992 <1447966060@qq.com>
2021-03-12 13:38:48 +0800

    3.12

--- pages/personalCentre/invoicingMessage/invoicingMessage.vue
+++ pages/personalCentre/invoicingMessage/invoicingMessage.vue
@@ -1,7 +1,28 @@
 <template>
 	<view class="invoicingMessage-app">
 		<view class="order-code">
-			订单号:
+			订单号:{{orderMessage.order_info.order_no}}
+		</view>
+		<view class="shop-detail">
+			<!-- <image :src="orderMessage.mch_info.cover_url" mode="" class="shop-logo"></image> -->
+			<image src="" mode="" class="shop-logo"></image>
+			<view class="shop-name">
+				<text>店铺名:{{orderMessage.mch_info.name}}</text>
+				<text>ID:{{orderMessage.mch_info.mch_id}}</text>
+			</view>
+		</view>
+		<view class="product-price">
+			价格:￥{{orderMessage.order_info.order_price}}
+		</view>
+		<view class="use-points flex flex-y-center flex-x-between">
+			<view>
+				使用抵扣券 :{{orderMessage.integral_max_deduction}}
+				<view class="xieti">拥有抵扣券金额:{{orderMessage.user_integral_num}}</view>
+			</view>
+			<switch :checked="is_integral" @change="useIntegral" :color='textColor' class="points-switch" />
+		</view>
+		<view class="go-pay">
+			<button type="default" @click='pay'>去支付</button>
 		</view>
 	</view>
 </template>
@@ -10,24 +31,64 @@
 	export default {
 		data() {
 			return {
+				orderMessage:{
+					"order_info":{
+						"id":1,
+						 "mch_id": 31,
+						 "order_no": "MS202103110906011010910457",
+						 "order_price": "100.00",
+					},
+					"mch_info":{
+						"mch_id": 31,
+						"name": "维也纳国际酒店(深圳草埔百鸽笼地铁站店)",
+						"cover_url": "http://",
+					},
+					"user_integral_num": 99999,
+					"integral_max_deduction": 100
+				},//结账单信息
+				textColor: '#bc0100',
+				is_integral:false,
 				
 			};
 		},
-		onLoad(options) {   //通过扫二维码进入此页面会带有一个id
-			this.$http.request({
-				url: this.$api.moreShop.getpayMessage,
-				method: 'POST',
-				data: {
-					id:options.id
-				},
-				showLoading: true
-			}).then(res => {
-				if(res.code==0){
-					console.log(res)
+		onLoad() {   //通过扫二维码进入此页面会带有一个id
+			this.getmessage()
+		},
+		methods:{
+			useIntegral(e) {
+				console.log(e)
+				this.is_integral = e.detail.value;
+				if(this.is_integral=false){
+					this.orderMessage.order_info.order_price=this.orderMessage.order_info.order_price
 				}else{
-					// this.$http.toast(res.msg)
+					this.orderMessage.order_info.order_price=Number(this.orderMessage.order_info.order_price)-Number(this.orderMessage.integral_max_deduction)
+					this.getmessage()
 				}
-			})
+				// this.is_integral ? this.use_integral = 1 : this.use_integral = 0; //是否使用抵扣券(请求用)
+			},
+			getmessage(){
+				let routes = getCurrentPages(); // 获取当前打开过的页面路由数组
+				let curParam = routes[routes.length - 1].options; //获取路由参数
+				this.$http.request({
+					url: this.$api.moreShop.getpayMessage,
+					method: 'POST',
+					data: {
+						id:curParam.id
+					},
+					showLoading: true
+				}).then(res => {
+					if(res.code==0){
+						this.orderMessage=res.data
+					}else{
+						this.$http.toast(res.msg)
+					}
+				})
+			},
+			pay(){ //点击去支付
+				uni.navigateTo({
+					url:'../invoicingMessage/pay?id='+this.orderMessage.order_info.id+"&use_integral="+this.orderMessage.integral_max_deduction
+				})
+			}
 		}
 	}
 </script>
@@ -36,4 +97,14 @@
 	.invoicingMessage-app{width: 100%;overflow: hidden;}
 	.order-code{width: 100%;height: 100rpx;background: #fff;margin: 30rpx 0 0 0;line-height: 100rpx;
 	font-weight: 600;padding: 0rpx 20rpx;color: #000;}
+	.shop-detail{width: 100%;overflow: hidden;background: #fff;margin-top: 20rpx;display: flex;padding: 0rpx 20rpx;}
+	.shop-logo{width: 158rpx;height: 158rpx;}
+	.shop-name{margin-left: 20rpx;width: 500rpx;}
+	.shop-name text{display: block;}
+	.shop-name text:nth-of-type(1){margin-top:25rpx ;}
+	.product-price{width: 100%;height: 100rpx;background: #fff;margin: 30rpx 0 0 0;line-height: 100rpx;
+	font-weight: 600;padding: 0rpx 20rpx;color: #000;}
+	.use-points{background: #fff;margin-top: 40rpx;}
+	.go-pay{width: 100%;height: 100rpx;margin-top: 100rpx;}
+	.go-pay button{width: 100%;outline: none;border: none;background: #19BE6B;color: #fff;}
 </style>
---
+++ pages/personalCentre/invoicingMessage/pay.vue
@@ -0,0 +1,405 @@
+<template>
+	<view class="cart-root">
+		<com-nav-bar @clickLeft="back" left-icon="back" title="账单支付" :status-bar="true" :background-color="navBg" :border="false"
+		 :color="navCol"></com-nav-bar>
+		<!-- 背景图，可自定义 -->
+		<view class="cart-bg" :style="{background:'url('+bg_url+')'}"></view>
+		<view class="mainContent">
+			<view class="up">
+				<view class="balance">
+					<text>账户余额:</text>
+
+					<view><text class="price">{{payData.balance}}</text>元</view>
+				</view>
+				<view class="pay-content">
+					<view class="pay-title">支付金额</view>
+					<view class="pay-price" :style="{color:textColor}" v-if="payData"><text class="pay-icon">&yen;</text>{{payData.amount}}</view>
+					<view class="numbering">订单编号:{{payData.orderNo}}</view>
+				</view>
+			</view>
+			<view class="down" v-if="payData.amount">
+				<view class="down-item" v-for="(item,index) in payData.supportPayTypes" :key="index">
+					<view class="item-left">
+						<image class="item-img" :src="img_url+'images/pay/'+item+'.png'" mode=""></image>
+						<text v-if="item == 'wechat'">微信支付</text>
+						<text v-if="item == 'balance'">余额支付</text>
+					</view>
+					<view class="item-right-box" @tap="switchIcon(index)">
+						<view v-if="index == switchIndex" class="item-icon iconfont icon-dagou1" :style="{color:textColor}"></view>
+						<view v-else class="item-right"></view>
+					</view>
+				</view>
+			</view>
+			<view class="down" v-else>
+				<view class="down-item">
+					<view class="item-left">
+						<image class="item-img" :src="img_url+'images/pay/'+'balance'+'.png'" mode=""></image>
+						<text>余额支付</text>
+					</view>
+					<view class="item-right-box">
+						<view class="item-icon iconfont icon-dagou1" :style="{color:textColor}"></view>
+					</view>
+				</view>
+			</view>
+
+			<view class="confirmPay" :style="{background:textColor}" @click="confirmPay">确认支付</view>
+		</view>
+	</view>
+</template>
+
+<script>
+	// #ifdef H5
+	var jweixin = require('jweixin-module');
+	// #endif
+	// #ifdef MP-WEIXIN || APP-PLUS
+	import {
+		setPay
+	} from '@/config/utils.js'
+	// #endif
+	export default {
+		data() {
+			return {
+				img_url: this.$api.img_url,
+				showBool: false, // 固定定位状态栏占位
+				opacity: 0,
+				payData: '',
+				switchIndex: 0,
+				textColor: "#BC0100",
+				bg_url: '',
+				navBg: '',
+				navCol: '',
+				id: '', //结账ID
+				use_integral: '', //折扣卷使用数量
+			}
+		},
+		onLoad(options) {
+			this.textColor = this.globalSet('textCol');
+			this.bg_url = this.globalSet('imgUrl');
+			this.navBg = this.globalSet('navBg');
+			this.navCol = this.globalSet('navCol');
+			if (options.id) {
+				this.id = options.id
+			}
+			if (options.use_integral) {
+				this.use_integral = options.use_integral
+			}
+			this.getPayData()
+
+		},
+		methods: {
+			back() {
+				this.navBack();
+			},
+			getPayData() { //获取支付信息
+				this.$http.request({
+					url: this.$api.moreShop.getpayINfo,
+					data: {
+						id: this.id,
+						use_integral: this.use_integral,
+					},
+					showLoading: true,
+				}).then((res) => {
+					if (res.code == 0) {
+						console.log(res.data)
+						this.payData = res.data;
+					}
+				})
+			},
+			switchIcon(index) { //支付模式切换
+				this.switchIndex = index;
+			},
+			confirmPay() { // 请求支付接口，如果是余额支付
+				let that = this
+				that.$http.request({
+					url: that.$api.payment.doPay,
+					method: 'post',
+					showLoading: true,
+					data: {
+						union_id: that.payData.union_id,
+						pay_type: that.payData.amount > 0 ? that.payData.supportPayTypes[that.switchIndex] : 'balance' //self.payData.supportPayTypes[self.switchIndex]
+					}
+				}).then(res => {
+					alert(11)
+					// if (res.code == 0) {
+					// 	var priceflag = that.payData.amount > 0 ? that.payData.supportPayTypes[that.switchIndex] : 'balance';
+					// 	if (priceflag == 'balance') {
+					// 		that.$http.toast('支付成功!');
+					// 		setTimeout(() => {
+					// 			uni.redirectTo({
+					// 				url: '../../index/index'
+					// 			})
+					// 		}, 500)
+					// 		return;
+					// 	}
+					// 	// #ifdef H5						
+					// 		that.$wechatSdk.pay(res.data, '/pages/index/index');
+					// 	// #endif
+					// }
+				})
+			}
+			// // 请求支付接口，如果是余额支付
+			// confirmPay(flag){
+			// 	let self = this; 
+			// 	let flagStatus = flag;
+			// 	// 这个是拼团跳转页面		
+			// 	let groupUrl = '/mch/group-buy/detail?goods_id='+self.goods_id+'&detailId='+self.active_id;
+			// 	let orderListUrl = '/mch/group-buy/orderList?goods_id='+self.goods_id;
+
+			// 	self.$http.request({
+			// 		url: self.$api.payment.doPay,
+			// 		showLoading: true,
+			// 		method: 'post',
+			// 		data: {
+			// 			union_id: self.payData.union_id,
+			// 			pay_type: self.payData.amount > 0 ? self.payData.supportPayTypes[self.switchIndex] : 'balance'    //self.payData.supportPayTypes[self.switchIndex]
+			// 		}
+			// 	}).then(res => {
+			// 		if (res.code == 0) {
+			// 			var priceflag = self.payData.amount > 0 ? self.payData.supportPayTypes[self.switchIndex] : 'balance';
+			// 			// #ifdef H5
+			// 			if(self.is_index==1){	//如果是拼团支付
+
+			// 				if(flagStatus){
+			// 					self.$wechatSdk.pay(res.data,groupUrl);
+			// 				}else{
+			// 					self.$wechatSdk.pay(res.data,orderListUrl);
+			// 				}	
+			// 			}else{
+			// 				self.$wechatSdk.pay(res.data,'/pages/order/list?status=1');	
+			// 			}
+			// 			// #endif
+
+			// 			// #ifdef MP-WEIXIN || APP-PLUS
+			// 			setPay(res.data, (result) => {
+			// 				console.log(12312);
+			// 				let _url = '/pages/order/list?status=1'
+			// 				if (result.success) {
+			// 					self.$http.toast("支付成功")
+			// 				} else {
+			// 					self.$http.toast("未支付")
+			// 					_url = '/pages/order/list?status=0'
+			// 				}
+
+			// 				setTimeout(() => {
+			// 					if(self.is_index==1){	//如果是拼团支付
+			// 						if(flagStatus){
+			// 							uni.redirectTo({
+			// 								url: groupUrl
+			// 							})
+			// 						}else{
+			// 							uni.redirectTo({
+			// 								url: orderListUrl
+			// 							})
+			// 						}
+			// 					}else{
+			// 						uni.redirectTo({
+			// 							url: _url
+			// 						})
+			// 					}
+			// 				},1000)
+
+			// 			});
+			// 			// #endif
+			// 		} else {
+			// 			self.$http.toast(res.msg);
+			// 			return
+			// 			setTimeout(() => {
+			// 				if(self.is_index==1){	//如果是拼团支付--跳转拼团订单列表
+			// 					uni.redirectTo({
+			// 						url: orderListUrl
+			// 					})
+			// 				}else{
+			// 					uni.redirectTo({
+			// 						url: '/pages/order/list?status=0'
+			// 					})
+			// 				}
+			// 			},1000)
+			// 		}
+			// 	})
+			// },
+
+
+
+
+
+
+
+		},
+	}
+</script>
+
+<style scoped>
+	.status_bar {
+		height: var(--status-bar-height);
+		width: 100%;
+	}
+
+	.status_bar2 {
+		height: var(--status-bar-height);
+		width: 100%;
+		position: fixed;
+		top: 0;
+		z-index: 99;
+	}
+
+	body,
+	page {
+		background-color: $uni-bg-color-grey;
+	}
+
+	.cart-root {
+		position: relative;
+		z-index: 1;
+	}
+
+	.cart-bg {
+		position: absolute;
+		width: 100%;
+		background-size: cover;
+		height: 320rpx;
+	}
+
+	.cart-bg-img {
+		width: 100%;
+	}
+
+	.cart-title {
+		padding: 40rpx 0 0rpx;
+		color: #FFFFFF;
+		font-size: 14pt;
+		letter-spacing: 4rpx;
+		text-align: center;
+		position: relative;
+		margin-bottom: 40rpx;
+	}
+
+	.back {
+		position: absolute;
+		top: 40rpx;
+		left: 30rpx;
+		font-size: 40rpx;
+	}
+
+	.mainContent {
+		z-index: 999;
+		position: relative;
+		width: 690rpx;
+		margin: 118rpx auto 0;
+	}
+
+	.up {
+		background: #FFFFFF;
+		border-radius: 15rpx;
+		margin-bottom: 30rpx;
+	}
+
+	.balance {
+		display: flex;
+		justify-content: space-between;
+		padding: 30rpx 23rpx;
+		font-size: 10pt;
+		color: #000000;
+		border-bottom: 2rpx solid #F2F2F2;
+	}
+
+	.price {
+		font-size: 13pt;
+		margin-right: 10rpx;
+	}
+
+	.pay-content {
+		padding: 43rpx 0;
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+	}
+
+	.pay-title {
+		font-size: 9pt;
+		font-weight: 600;
+		margin-bottom: 20rpx;
+		color: #000000;
+	}
+
+	.pay-icon {
+		font-size: 12pt;
+		margin-right: 20rpx;
+		font-weight: 500;
+	}
+
+	.pay-price {
+		color: #BC0100;
+		font-size: 16pt;
+		font-weight: 600;
+		margin-bottom: 20rpx;
+	}
+
+	.numbering {
+		font-size: 10pt;
+		color: #000000;
+	}
+
+	.down {
+		background: #FFFFFF;
+		border-radius: 15rpx;
+		padding: 40rpx 20rpx;
+	}
+
+	.down-item {
+		display: flex;
+		justify-content: space-between;
+		align-items: center;
+		margin-bottom: 40rpx;
+	}
+
+	.down .down-item:last-child {
+		margin-bottom: 0rpx;
+	}
+
+	.item-left {
+		display: flex;
+		align-items: center;
+		color: #000000;
+		font-size: 10pt;
+	}
+
+	.item-img {
+		width: 40rpx;
+		height: 40rpx;
+		margin-right: 30rpx;
+	}
+
+	.item-right-box {
+		width: 38rpx;
+		height: 38rpx;
+		position: relative;
+	}
+
+	.item-right {
+		width: 38rpx;
+		height: 38rpx;
+		border-radius: 50%;
+		border: 2rpx solid #8B8B8B;
+		position: absolute;
+	}
+
+	.item-icon {
+		font-size: 38rpx;
+		position: absolute;
+		top: 0rpx;
+		left: 0rpx;
+		line-height: 40rpx;
+		color: #BC0100;
+	}
+
+	.confirmPay {
+		color: #FFFFFF;
+		background: #bc0100;
+		padding: 26rpx 0;
+		text-align: center;
+		border-radius: 80rpx;
+		font-size: 10pt;
+		letter-spacing: 4rpx;
+		margin-top: 80rpx;
+	}
+</style>
