697fd2d61e314626dd0c5139d0da92cdcf8f860b
z-xing-1992 <1447966060@qq.com>
2021-03-13 15:20:53 +0800

    33

--- pages/personalCentre/accountingOrder/accountingOrder.vue
+++ pages/personalCentre/accountingOrder/accountingOrder.vue
@@ -14,7 +14,9 @@
 </template>
 
 <script>
-	// var jweixin = require('jweixin-module');
+	//#ifdef H5
+		var jweixin = require('jweixin-module');
+	//#endif
 	export default {
 		data() {
 			return {
@@ -26,18 +28,18 @@
 			this.$wechatSdk.initJssdk(function(signData){
 				console.log(signData)
 			});
-			//#endif
+		//#endif
 		},
 		methods: {
 			ercodeBtn(){
 				//#ifdef H5
-				jweixin.scanQRCode({
-				  needResult: 0, // 默认为0，扫描结果由微信处理，1则直接返回扫描结果，
-				  scanType: ["qrCode","barCode"], // 可以指定扫二维码还是一维码，默认二者都有
-				  success: function (res) {
-				    var result = res.resultStr; // 当needResult 为 1 时，扫码返回的结果
-				  }
-				});
+					jweixin.scanQRCode({
+					  needResult: 0, // 默认为0，扫描结果由微信处理，1则直接返回扫描结果，
+					  scanType: ["qrCode","barCode"], // 可以指定扫二维码还是一维码，默认二者都有
+					  success: function (res) {
+						var result = res.resultStr; // 当needResult 为 1 时，扫码返回的结果
+					  }
+					});
 				//#endif
 			},
 			
