5cc26925cd5aee202c737f995163b466855bb618
z-xing-1992 <1447966060@qq.com>
2021-03-29 17:32:24 +0800

    3.29

--- pages/supplement/supplement.vue
+++ pages/supplement/supplement.vue
@@ -518,11 +518,10 @@
 				let year = date.getFullYear();
 				let month = date.getMonth() + 1;
 				let day = date.getDate();
-
 				if (type === 'start') {
 					year = year - 60;
 				} else if (type === 'end') {
-					year = year + 2;
+					year = year + 40;
 				}
 				month = month > 9 ? month : '0' + month;;
 				day = day > 9 ? day : '0' + day;
