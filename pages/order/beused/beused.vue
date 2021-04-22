55d2ec814f2a748bd334164a107d48bb4a5a6267
zx-1992 <1447966060@qq.com>
2021-04-22 15:59:00 +0800

    4.22

--- pages/order/beused/beused.vue
+++ pages/order/beused/beused.vue
@@ -208,7 +208,6 @@
 							} = res.data;
 							
 							this.dataList = key != 'refresh' ? this.dataList.concat(list) : list; // key != refresh 则是加载下一页数据
-							console.log(this.dataList)
 							this.pages = pagination;
 							this.pullUpOn = true;
 						}
