e8e0eff85c0f6781c5d5027d118da1a37f1d5855
zx-1992 <1447966060@qq.com>
2021-06-18 17:21:44 +0800

    6.18

--- pages/hotel/orderList/orderList.vue
+++ pages/hotel/orderList/orderList.vue
@@ -1,6 +1,10 @@
 <template>
-	<view>
-		
+	<view class="hotel-orderList-app">
+		<view class="hotel-orderList-status">
+			<view v-for="(item,index) in list" :key='index' :class="switchIndex==index?'active':''" @click="checkIndex(index,item.status)">
+				{{item.title}}
+			</view>
+		</view>
 	</view>
 </template>
 
@@ -8,12 +12,46 @@
 	export default {
 		data() {
 			return {
-				
+				list:[
+					{
+						title:'全部',
+						status:0
+					},
+					{
+						title:'待付款',
+						status:1
+					},
+					{
+						title:'待处理',
+						status:2
+					},
+					{
+						title:'待出行',
+						status:3
+					},
+					// {
+					// 	title:'待评价',
+					// 	status:4
+					// },
+					{
+						title:'退款/售后',
+						status:4
+					}
+				],
+				switchIndex:0
 			};
+		},
+		methods:{
+			checkIndex(index,status){
+				this.switchIndex=index
+			}
 		}
 	}
 </script>
 
 <style lang="less">
-
+	.hotel-orderList-app{width: 100%;overflow: hidden;}
+	.hotel-orderList-status{width: 100%;height: 80rpx;display: flex;justify-content: space-evenly;}
+	.hotel-orderList-status view{font-size: 26rpx;line-height: 80rpx;width: 16%;text-align: center;}
+	.active{color: rgb(255, 113, 4);border-bottom: 6rpx solid  rgb(255, 113, 4);font-weight: bold;}
 </style>
