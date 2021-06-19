1b7d7988d03e09caa43d1d17af786f810322eee5
zx-1992 <1447966060@qq.com>
2021-06-19 15:04:23 +0800

    6.19

--- pages/user/examine/examine.vue
+++ pages/user/examine/examine.vue
@@ -1,8 +1,8 @@
 <template>
 	<view class="examine_app">
 		<view class="search">
-			<input type="search" value="" placeholder="关键字搜索 店铺名、手机号"/>
-			<view class="search_check">
+			<input type="search" v-model="form.keyword" placeholder="关键字搜索 店铺名"/>
+			<view class="search_check" @click="search">
 				搜索
 			</view>
 		</view>
@@ -13,15 +13,34 @@
 			</view>
 		</view>
 		<view class="examine-list">
-			<view class="examine-item">
+			<view class="examine-item" v-for="(item,index) in list" :key='index'>
 				<view class="examine-item-title">
 					<view class="examine-item-title-name">
-						早点见面
+						{{item.store.name}}
 					</view>
 					<view class="examine-item-title-name-time-status">
-						<text></text>
-						<text>待审核</text>
+						<text>提交时间：{{item.created_at}}</text>
+						<text v-if="form.review_status==0">待审核</text>
+						<text v-if="form.review_status==1" style="background: #16AB60;">已通过</text>
+						<text v-if="form.review_status==2" style="background: red;">未通过</text>
+						<text v-if="form.review_status==0" style="float: right;">去审核</text>
 					</view>
+				</view>
+				<view class="userMessage">
+					<view>
+						<image :src="item.store.cover_url" mode=""></image>
+					</view>
+					<view>
+						<text>{{item.realname}}</text>
+					</view>
+					<view>
+						<text>{{item.mobile}}</text>
+					</view>
+				</view>
+				<view class="recommend">
+					<text>推荐人:{{item.parent_nickname}}</text>
+					<text>{{item.parent_mobile}}</text>
+					<text>{{item.parent_role_type}}</text>
 				</view>
 			</view>
 		</view>
@@ -46,13 +65,68 @@
 						review_status:2
 					}
 				],
-				statusIndex:'',
+				statusIndex:0,
+				form:{
+					page:1,
+					review_status:0,//0待审核1通过2未通过
+					keyword:''
+				},
+				list:[],
+				page_count:'',
 			};
+		},
+		onLoad() {
+			this.getAuditList() 
 		},
 		methods:{
 			swichIndex(index,review_status){ //选择状态
 				this.statusIndex=index
+				this.form={
+					page:1,
+					review_status:review_status,//0待审核1通过2未通过
+				}
+				this.list=[]
+				this.page_count=''
+				this.getAuditList()
+			},
+			getAuditList(){
+				this.$http
+					.request({
+						url: this.$api.moreShop.getAudit,
+						method: 'GET',
+						data:this.form,
+						showLoading: true
+					})
+					.then(res => {
+						if(res.code==0){
+							console.log(res.data)
+							if(res.data.list.length==0)return false
+							let list= res.data.list;
+							var arr=this.list.concat(list)
+							this.list=arr
+							this.page_count= res.data.pagination.page_count;
+						}
+				});
+			},
+			search(){
+				let keyword=this.form.keyword
+				let review_status=this.statusIndex
+				this.form={
+					page:1,
+					review_status:review_status,
+					keyword:keyword
+				}
+				this.list=[]
+				this.page_count=''
+				this.getAuditList()
 			}
+		},
+		onReachBottom() {
+			if(this.form.page==this.page_count){
+				return false;
+			} 		
+			this.form.page=this.form.page+1
+			this.getAuditList();
 		}
 	}
 </script>
@@ -68,7 +142,22 @@
     .examine-status view{height: 60rpx;margin: 10rpx 0;font-size: 30rpx;}
 	.active{color: #ff7104;border-bottom: 4rpx solid #ff7104;}
 	.examine-list{width: 100%;overflow: hidden;}
-	.examine-item{width: 80%;height: 300rpx;background: #fff;margin: 20rpx auto;border-radius: 20rpx;
+	.examine-item{width: 80%;height: 310rpx;background: #fff;margin: 30rpx auto;border-radius: 20rpx;
 	box-shadow: 0px 3px 10px rgba(0, 0, 0, 0.3);padding: 10rpx;}
+	.examine-item-title{width: 100%;overflow: hidden;}
+	.examine-item-title-name{font-size: 28rpx;color: #000;text-align: center;margin: 5rpx 0 10rpx 0;}
+	.examine-item-title-name-time-status {font-size: 25rpx;}
+	.examine-item-title-name-time-status text{display: inline-block;}
+	.examine-item-title-name-time-status text:nth-of-type(2){background: #ff7104;color: #fff;width: 100rpx;text-align: center;border-radius: 30rpx;margin-left: 20rpx;}
+	.examine-item-title-name-time-status text:nth-of-type(3){float: right;height: 50rpx;width: 100rpx;background: #ff7104;line-height: 50rpx;text-align: center;border-radius: 30rpx;color: #fff;}
+	.userMessage{width: 100%;overflow: hidden;display: flex;justify-content: space-between;margin-top: 10rpx}
+	.userMessage image{width: 120rpx;height: 120rpx;display: block;border-radius: 50%;}
+	.userMessage view text{display: block;height: 120rpx;text-align: center;line-height: 120rpx;color: #000;}
+	.recommend{width: 100%;overflow: hidden;display: flex;justify-content: space-between;}
+	.recommend text{display: inline-block;font-size: 25rpx;height: 60rpx;line-height: 60rpx;width: 33%;text-align: center;
+	white-space: nowrap;
+	text-overflow: ellipsis;
+	overflow: hidden;
+	}
 </style>
 
