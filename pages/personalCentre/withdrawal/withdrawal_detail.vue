00466f3766ac4552a60a92127a0abf5bbf9723fb
zx-1992 <1447966060@qq.com>
2021-06-22 11:16:04 +0800

    6.22

--- pages/personalCentre/withdrawal/withdrawal_detail.vue
+++ pages/personalCentre/withdrawal/withdrawal_detail.vue
@@ -3,7 +3,6 @@
 		
 	</view>
 </template>
-
 <script>
 	export default{
 		data(){
@@ -12,10 +11,25 @@
 			}
 		},
 		onLoad() {
-			
+			this.getwithdrawal_detail()
 		},
 		methods:{
-			
+			getwithdrawal_detail(){
+				this.$http
+					.request({
+						url: this.$api.moreShop.withdrawal_detail,
+						method: 'GET',
+						showLoading: true
+					})
+					.then(res => {
+						console.log(res.data)
+						if(res.code==0){
+							
+						}else{
+							this.$http.toast(res.msg);
+						}
+					});
+			}
 		}
 	}
 </script>
