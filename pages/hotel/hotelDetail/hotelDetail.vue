ec3915ad7d76348b3980ab5dac18a8d3d7fd46a8
zx-1992 <1447966060@qq.com>
2021-06-28 12:00:57 +0800

    6.28

--- pages/hotel/hotelDetail/hotelDetail.vue
+++ pages/hotel/hotelDetail/hotelDetail.vue
@@ -1,19 +1,20 @@
 <template>
 	<view class="hotel-detail-app">
 		<view class="hotel-detai-header">
-			<text>已确认</text>
-			<text>房间将为您整晚保留，请安心入住</text>
+			<text>{{orderDetail.order.status_text}}</text>
+		<!-- 	<text>房间将为您整晚保留，请安心入住</text> -->
 		</view>
 		<view class="hotel-detai-setAgain">
-			<text>取消订单</text>
-			<!-- <text>发票/报销</text> -->
-			<text>再次预订</text>
+			<text v-if="orderDetail.action.is_payable==1">去支付</text>
+			<text v-if="orderDetail.action.is_cancelable==1" @click="cancleOrder(orderDetail.order.id)">取消订单</text>
+			<text v-if="orderDetail.action.is_refundable==1">退款/售后</text>
+			<!-- <text>再次预订</text> -->
 		</view>
 		<view class="hotel-detail-prompt">
-			<view class="hotel-detail-prompt-title">
+			<!-- <view class="hotel-detail-prompt-title">
 				<text>免费取消</text>
 				<text>预定后可随时取消订单，补商汇不收取任何费用</text>
-			</view>
+			</view> -->
 			<view class="hotel-detail-prompt-detail">
 				<view class="hotel-detail-prompt-detail-title">
 					<image :src="img_url+'/hotel/prompt-logo.png'" mode=""></image>
@@ -29,25 +30,25 @@
 		</view>
 		<view class="hotel-detail">
 			<view class="hotel-detail-name">
-				<image :src="img_url+'/shoplogo.png'" mode="" style="width: 24rpx;height: 24rpx;margin-top: 10rpx;"></image>
-				<text>酒店名字中心城市</text>
-				<image :src="img_url+'/hotel/right.png'" mode="" style="width: 10rpx;height: 16rpx;margin-top: 15rpx;"></image>
+				<image :src="img_url+'/shoplogo.png'" mode="" style="width: 100rpx;height: 24rpx;margin-top: 10rpx;"></image>
+				<!-- <text>{{orderDetail.hotel.name}}</text> -->
+				<!-- <image :src="img_url+'/hotel/right.png'" mode="" style="width: 10rpx;height: 16rpx;margin-top: 15rpx;"></image> -->
 			</view>
 			<view class="hotel-detail-product">
 				<view class="hotel-detail-product-image">
-					<image src="../../../plugins/images/extensions/o2o/shuiguotu.png" mode=""></image>
+					<image :src="orderDetail.hotel.thumb_url" mode=""></image>
 				</view>
 				<view class="hotel-detail-product-messgae">
 					<view class="hotel-detail-product-messgae-name">
-						加州国际酒店（广州白云机场店） 
+						{{orderDetail.hotel.name}} 
 					</view>
-					<view class="hotel-detail-product-messgae-facilities">
+					<!-- <view class="hotel-detail-product-messgae-facilities">
 						<text>儿童乐园</text>
 						<text>娱乐场/棋牌室</text>
-					</view>
+					</view> -->
 					<view class="hotel-phone-address">
-						<text style="margin-bottom: 5rpx;">酒店联系方式 : 121-5255623</text>
-						<text>地址 :成都市里面免费看了吗分了吗发热快乐反人类看门狗</text>
+						<text style="margin-bottom: 5rpx;">酒店联系方式 : {{orderDetail.hotel.contact_phone}}</text>
+						<text>地址 :{{orderDetail.hotel.province_name}}{{orderDetail.hotel.city_name}}{{orderDetail.hotel.address}}</text>
 					</view>
 				</view>
 			</view>
@@ -57,31 +58,37 @@
 			</view>
 			<view class="hotel-room-detail">
 				<view class="hotel-room-detail-messahe">
-					豪华大套间-24小时免费接机:车程10分钟
-					有早餐·1间·1晚
+					{{orderDetail.room.name}}-
+					<text v-if="orderDetail.hotel.policy_breakfast==0">无早餐</text>
+					<text v-if="orderDetail.hotel.policy_breakfast==1">有早餐</text>
+					·{{orderDetail.order.booking_num}}间·{{orderDetail.order.booking_days}}天
 				</view>
 				<view class="hotel-room-detail-specifications">
-					28m²|大床1.8m |可住2人
+					{{orderDetail.room.room_size}}m²|
+					<text v-if="orderDetail.room.bed_type=='single'">单床</text>
+					<text v-if="orderDetail.room.bed_type=='double'">双床</text>
+					<text v-if="orderDetail.room.bed_type=='double'">大床</text>
+					{{orderDetail.room.bed_width}}m |可住{{orderDetail.room.people_num}}人
 				</view>
 				<view class="hotel-room-detail-time">
 					<view>
-						<text style="color: #000;">6月18日  周五  </text>
-						<text>08:00后入住</text>
+						<text style="color: #000;">{{orderDetail.order.booking_start_date}}</text>
+						<text>入住</text>
 					</view>
 					<view>
-						<text style="color: #000;">6月19日  周六    </text>
+						<text style="color: #000;">{{orderDetail.order.end_date}}</text>
 						<text>14:00前离店</text>
 					</view>
 				</view>
 			</view>
-			<view class="passengers-message">
+			<view class="passengers-message" v-for="(item,index) in orderDetail.order.booking_passengers" :key='index'>
 				<view>
 					<text>入住人</text>
-					<text>姓名</text>
+					<text>{{item.name}}</text>
 				</view>
 				<view>
 					<text>联系手机</text>
-					<text>11位电话号码</text>
+					<text>{{item.mobile}}</text>
 				</view>
 			</view>
 		</view>
@@ -93,7 +100,55 @@
 		data() {
 			return {
 				img_url: this.$api.img_url,
+				orderDetail:'',
+				hotel_order_id:'',
 			};
+		},
+		onLoad(options) {
+			if(options&&options.hotel_order_id){
+				this.hotel_order_id=options.hotel_order_id
+				this.getOrderDetail(options.hotel_order_id)
+			}
+		},
+		methods:{
+			getOrderDetail(hotel_order_id){//获取订单详情
+				this.$http
+					.request({
+						url: this.$api.hotel.getOrderdetail,
+						method: 'POST',
+						data:{
+							hotel_order_id:hotel_order_id
+						},
+						showLoading: true
+					})
+					.then(res => {
+						if(res.code==0){
+							this.orderDetail=res.data
+						}else{
+							this.$http.toast(res.msg);
+						}
+				});
+			},
+			cancleOrder(id){
+				this.$http
+					.request({
+						url: this.$api.hotel.cancleOrder,
+						method: 'POST',
+						data:{
+							hotel_order_id:id
+						},
+						showLoading: true
+					})
+					.then(res => {
+						if(res.code==0){
+							this.$http.toast('取消订单成功');
+							this.getOrderDetail(this.hotel_order_id)
+						}else{
+							this.$http.toast(res.msg);
+						}
+				});
+			}
+		
 		}
 	}
 </script>
@@ -143,7 +198,7 @@ display: -webkit-box;
 .hotel-room-detail-messahe{font-size: 28rpx;color: #000;width: 80%;margin-bottom: 10rpx;margin-top: 20rpx;}
 .hotel-room-detail-specifications{font-size: 25rpx;margin-bottom: 15rpx;}
 .hotel-room-detail-time{width: 100%;overflow: hidden;display: flex;justify-content: space-between;font-size: 25rpx;}
-.passengers-message{width: 90%;overflow: hidden;margin: 0 auto 80rpx;}
+.passengers-message{width: 90%;overflow: hidden;margin: 0 auto 20rpx;border: 1rpx solid #E6E6E6;;border-radius: 30rpx;padding: 20rpx;}
 .passengers-message view{width: 100%;height: 70rpx;line-height: 70rpx;}
 .passengers-message view text{display: inline-block;}
 .passengers-message view text:nth-of-type(1){font-size: 28rpx;width: 150rpx;}
