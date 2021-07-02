4047d0864c1d95bc1abaa6afc931d9ccd1ca2186
zx-1992 <1447966060@qq.com>
2021-07-02 15:56:02 +0800

    7.2

--- pages/hotel/hotel.vue
+++ pages/hotel/hotel.vue
@@ -285,6 +285,8 @@
 						that.form.lng=res.longitude
 						that.form.lat=res.latitude
 						that.form.city_id=0
+						uni.setStorageSync('x-longitude',res.longitude)
+						uni.setStorageSync('x-latitude',res.latitude)
 						that.recommendedForm.city_id=""
 						that.recommendedForm.lng=String(res.longitude)
 						that.recommendedForm.lat=String(res.latitude)
