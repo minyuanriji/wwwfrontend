bb2eba02ec00812d796047fa2266b670c683ddc2
zx-1992 <1447966060@qq.com>
2021-07-02 15:32:06 +0800

    7.2

--- pages/hotel/hotel.vue
+++ pages/hotel/hotel.vue
@@ -281,10 +281,13 @@
 				var that = this
 				uni.chooseLocation({
 					success: function(res) {
+						console.log(res)
 						that.text=res.address
 						that.form.lng=res.longitude
 						that.form.lat=res.latitude
 						that.form.city_id=0
+						uni.setStorageSync('x-longitude',res.longitude)
+						uni.setStorageSync('x-latitude',res.latitude)
 						that.recommendedForm.lng=String(res.longitude)
 						that.recommendedForm.lat=String(res.latitude)
 						that.getrecommended()
