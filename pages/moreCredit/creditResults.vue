5c2b2ed4ad627be2d3ddb7849b81723e8611a865
zx-1992 <1447966060@qq.com>
2021-07-17 15:06:07 +0800

    7.17

--- pages/moreCredit/creditResults.vue
+++ pages/moreCredit/creditResults.vue
@@ -35,6 +35,6 @@
 	.creditResult_app-detail{width: 100%;height: 300rpx;position: absolute;top: 0;left: 0;right: 0;bottom: 0;margin: auto;}
 	.creditResult_app-detail view{width: 100%;overflow: hidden;text-align: center;line-height: 88rpx;color: #fff;}
 	.creditResult_app-detail view image{display: inline-block;width: 92rpx;height: 88rpx;margin-right: 10rpx;}
-	.creditResult_app-detail view text{display: inline-block;height: 88rpx;line-height: 88rpx;color: #fff;font-size: 40rpx;font-weight: bold;}
+	.creditResult_app-detail view text{display: block;height: 88rpx;line-height: 88rpx;color: #fff;font-size: 40rpx;font-weight: bold;}
 	.creditResult_app-detail button{width: 60%;height: 80rpx;line-height: 80rpx;margin-top: 50rpx;outline: none;border: none;}
 </style>
