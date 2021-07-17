f531b5d5677de5ecec17f220b028ce6b45b39122
zx-1992 <1447966060@qq.com>
2021-07-17 16:02:26 +0800

    7.17

--- pages/moreCredit/creditResults.vue
+++ pages/moreCredit/creditResults.vue
@@ -15,26 +15,70 @@
 	</view>
 </template>
 <script>
-	export default{
-		data(){
-			return{
+	export default {
+		data() {
+			return {
 				img_url: this.$api.img_url,
 			}
 		},
-		methods:{
-			linkTo(){
+		methods: {
+			linkTo() { //点击确定跳转页面
 				uni.redirectTo({
-					url:'../index/index'
+					url: '../index/index'
 				})
 			}
 		}
 	}
 </script>
 <style lang="less">
-	.creditResult_app{width: 100%;height: 100%;background: #ff7104;position: relative;}
-	.creditResult_app-detail{width: 100%;height: 300rpx;position: absolute;top: 0;left: 0;right: 0;bottom: 0;margin: auto;}
-	.creditResult_app-detail view{width: 100%;overflow: hidden;text-align: center;line-height: 88rpx;color: #fff;}
-	.creditResult_app-detail view image{display: inline-block;width: 92rpx;height: 88rpx;margin-right: 10rpx;}
-	.creditResult_app-detail view text{display: block;height: 88rpx;line-height: 88rpx;color: #fff;font-size: 40rpx;font-weight: bold;}
-	.creditResult_app-detail button{width: 60%;height: 80rpx;line-height: 80rpx;margin-top: 50rpx;outline: none;border: none;}
+	.creditResult_app {
+		width: 100%;
+		height: 100%;
+		background: #ff7104;
+		position: relative;
+	}
+
+	.creditResult_app-detail {
+		width: 100%;
+		height: 300rpx;
+		position: absolute;
+		top: 0;
+		left: 0;
+		right: 0;
+		bottom: 0;
+		margin: auto;
+	}
+
+	.creditResult_app-detail view {
+		width: 100%;
+		overflow: hidden;
+		text-align: center;
+		line-height: 88rpx;
+		color: #fff;
+	}
+
+	.creditResult_app-detail view image {
+		display: inline-block;
+		width: 92rpx;
+		height: 88rpx;
+		margin-right: 10rpx;
+	}
+
+	.creditResult_app-detail view text {
+		display: block;
+		height: 88rpx;
+		line-height: 88rpx;
+		color: #fff;
+		font-size: 40rpx;
+		font-weight: bold;
+	}
+
+	.creditResult_app-detail button {
+		width: 60%;
+		height: 80rpx;
+		line-height: 80rpx;
+		margin-top: 50rpx;
+		outline: none;
+		border: none;
+	}
 </style>
