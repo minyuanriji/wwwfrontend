91f427957b48db1c0ebd01eb81ac8cf2235b0dc5
林工 <746027209@qq.com>
2021-10-11 17:42:56 +0800

    17.42

--- common/api.js
+++ common/api.js
@@ -1,12 +1,12 @@
 // 正式服务器
-let api_root = 'https://www.mingyuanriji.cn/web/index.php?r=';
-let url = 'https://www.mingyuanriji.cn/web/static/'; //图片路径
-let plugins_img_url = 'https://www.mingyuanriji.cn/web/plugins/'; //扩展页面图片路径
+//let api_root = 'https://www.mingyuanriji.cn/web/index.php?r=';
+//let url = 'https://www.mingyuanriji.cn/web/static/'; //图片路径
+//let plugins_img_url = 'https://www.mingyuanriji.cn/web/plugins/'; //扩展页面图片路径
 
 // 测试服务器
-//let api_root = 'https://dev.mingyuanriji.cn/web/index.php?r=';
-//let url = 'https://dev.mingyuanriji.cn/web/static/'; //图片路径
-//let plugins_img_url = 'https://dev.mingyuanriji.cn/web/plugins/'; //扩展页面图片路径
+let api_root = 'https://dev.mingyuanriji.cn/web/index.php?r=';
+let url = 'https://dev.mingyuanriji.cn/web/static/'; //图片路径
+let plugins_img_url = 'https://dev.mingyuanriji.cn/web/plugins/'; //扩展页面图片路径
 
 
 // 本地服务器
