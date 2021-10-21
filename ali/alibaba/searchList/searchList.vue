5afccde096aa3cd5f45b5646b33df7ab4dc63b27
zx-1992 <1447966060@qq.com>
2021-10-21 13:58:28 +0800

    10.21

--- ali/alibaba/searchList/searchList.vue
+++ ali/alibaba/searchList/searchList.vue
@@ -2,7 +2,7 @@
 	<view class="ali-home-app">
 		<view class="ali-home-app-product">
 			<view class="product-item" v-for="(item,index) in goodsList" :key='index' @click="link(item.id)">
-				<image :src="item.cover_url" mode="widthFix" class="product-item-logo"></image>
+				<image :src="item.cover_url" mode="scaleToFill" class="product-item-logo"></image>
 				<view class="product-item-name">{{item.name}}</view>
 				<view class="product-item-money-buy">
 					<view class="product-item-money">
@@ -103,6 +103,8 @@
 	
 	.product-item-logo {
 		width: 100%;
+		height: 320rpx;
+		display: block;
 	}
 	.product-item-name {
 		width: 100%;
