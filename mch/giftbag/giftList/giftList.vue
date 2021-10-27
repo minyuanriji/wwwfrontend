59416c533643f150145a1807cdd54649dfeac766
zx-1992 <1447966060@qq.com>
2021-10-27 13:40:50 +0800

    10.27

--- mch/giftbag/giftList/giftList.vue
+++ mch/giftbag/giftList/giftList.vue
@@ -1,5 +1,24 @@
 <template>
 	<view class="giftList-app">
+		<!--搜索-->
+		<view class="index1_content_top" :style="{background:'#FF7104'}" id="index1_content_top">
+			<view style="width: 20%;color: #fff;font-size: 30rpx;font-weight: bold;overflow: hidden;
+			text-overflow:ellipsis;white-space: nowrap;" @click="setCITY">
+				<view v-if="city != ''">{{city}}</view>
+				<view v-else>加载中...</view>
+			</view>
+			<view class="index1_content_top_l_r"></view>
+			<view class="index1_content_top_r">
+				<view class="iconfont iconsousuo"></view>
+				<input type="text" placeholder="输入商家名、品类或商圈" class="index1_content_top_r_input" disabled
+					@click="search"></input>
+			</view>
+		</view>
+		<!---->
+		<view style="width: 100%;height: 120rpx;"></view>
+		
+		
+		
 		<view class="giftList-item" v-for="(item,index) in productList" :key='index' @click="linkDetail(item.id)">
 			<view class="giftList-item-left">
 				<image :src="item.cover_pic" mode="" class="giftLogo"></image>
@@ -29,15 +48,18 @@
 			<image :src="img_url+'/giftOrder_logo.png'" mode=""></image>
 			<text>暂无服务内容</text>
 		</view>
+		<citySelect @back_city="back_city" v-if="show"></citySelect>
 		<backTop :src="backTop.src"  :scrollTop="backTop.scrollTop"></backTop>
 	</view>
 </template>
 
 <script>
 	import backTop from '@/components/back-top/back-top.vue';
+	import citySelect from '@/components/linzq/linzq-city.vue';
 	export default {
 		components: {
-			backTop
+			backTop,
+			citySelect
 		},
 		data() {
 			return {
@@ -49,10 +71,24 @@
 					src: '../../../static/back-top/top.png',
 					scrollTop: 0
 				},
+				city: '',
+				show:false,
+				city_id:'',
+				keywords:'',
 			};
 		},
+		onReady() {
+			var info = uni.getSystemInfoSync()
+			var window_height = info.windowHeight
+			var query = uni.createSelectorQuery()
+			var that = this
+			query.select("#index1_content_top").boundingClientRect(function(res) {
+				var view_height = window_height - res.height
+				that.height = view_height
+			}).exec()
+		},
 		onLoad() {
-			this.packageList()
+			this.getmyLOcation()
 		},
 		methods:{
 			linkDetail(id){//跳转到大礼包详情
@@ -60,16 +96,33 @@
 					url:'../gifebagDetail/newgifebagDetail?pack_id='+id
 				})
 			},
+			back_city(e) { //城市选择回显
+			console.log(e)
+				if (e !== 'no') {
+					this.city = e.name;
+					
+					this.show = false;
+				} else {
+					this.show = false;
+				}
+			},
+			setCITY() { //选择城市
+				this.show = true
+			},
 			packageList(){ //获取大礼包列表
 				this.$http.request({
 					url: this.$api.package.getpackageList,
 					method: 'get',
 					data: {
-						page:this.page
+						page:this.page,
+						city_id:this.city_id,
+						keywords:this.keywords,
 					},
 					showLoading: true
 				}).then(res => {
 					if (res.code == 0) {
+						this.city=res.city_data.district
+						uni.setStorageSync("giftCity",res.city_data.district)
 						if(res.data.list.length==0)return false
 						let list= res.data.list;
 						var arr=this.productList.concat(list)
@@ -79,7 +132,35 @@
 						this.$http.toast(res.msg);
 					}
 				});
-			}
+			},
+			getmyLOcation() {
+				let that = this				
+				//#ifdef H5
+				that.$unifylocation.locationH5()
+				setTimeout(() => {
+					if(uni.getStorageSync('x-longitude-new'),uni.getStorageSync('x-latitude-new')){
+						uni.setStorageSync('x-longitude',uni.getStorageSync('x-longitude-new'))
+						uni.setStorageSync('x-latitude',uni.getStorageSync('x-latitude-new'))
+					}
+				}, 500)
+				setTimeout(() => {
+					this.packageList()
+				}, 1000)
+				// #endif
+				
+				// #ifndef H5
+				that.$unifylocation.locationMp()
+				setTimeout(() => {
+					if(uni.getStorageSync('x-longitude-new'),uni.getStorageSync('x-latitude-new')){
+						uni.setStorageSync('x-longitude',uni.getStorageSync('x-longitude-new'))
+						uni.setStorageSync('x-latitude',uni.getStorageSync('x-latitude-new'))
+					}
+				}, 500)
+				setTimeout(() => {
+					this.packageList()
+				}, 1000)
+				// #endif
+			},
 		},
 		onPageScroll(e) {
 			console.log(e)
@@ -96,7 +177,53 @@
 </script>
 
 <style lang="less">
+	@import url("../../../plugins/font-icon/iconfont1.css");
 	.giftList-app{width: 100%;overflow: hidden;}
+	.index1_content_top {
+		width: 100%;
+		display: flex;
+		align-items: center;
+		padding: 15px 15px 10px 15px;
+		position: fixed;
+		top: 88rpx;
+		left: 0;
+		z-index: 999;
+	}
+	
+	.index1_content_top_l {
+		display: flex;
+		font-size: 13px;
+		font-weight: bold;
+		color: #131313;
+	}
+	
+	.index1_content_top_l_r {
+		margin-left: 5px;
+		width: 8px;
+		height: 8px;
+		border-top: 2px #FFF solid;
+		border-right: 2px #fff solid;
+		transform: rotate(135deg);
+		margin-top: 3px;
+		margin-bottom: 8px;
+	}
+	
+	.index1_content_top_r {
+		width: 60%;
+		height: 29px;
+		background: #F4F4F4;
+		border-radius: 14px;
+		font-size: 14px;
+		margin-left: 15px;
+		display: flex;
+		align-items: center;
+		padding-left: 15px;
+	}
+	
+	.index1_content_top_r_input {
+		width: 100%;
+		font-size: 14px;
+	}
 	.giftList-item{width: 98%;overflow: hidden;display: flex;justify-content: space-evenly;margin: 10rpx auto;padding: 20rpx 0rpx;
 	background: #FFFFFF;box-shadow: 0px 3px 10px rgba(0, 0, 0, 0.4);border-radius: 20rpx;}
 	.giftList-item-left{width: 70%;display: flex;justify-content: space-between;}
