ebdfb79c23a182183ee6ba53aebda5401c00e1e9
zx-1992 <1447966060@qq.com>
2021-11-10 11:54:46 +0800

    11.10

--- ali/alibaba/orderList/orderList.vue
+++ ali/alibaba/orderList/orderList.vue
@@ -1,5 +1,26 @@
 <template>
 	<view class="container">
+		<view class="tui-searchbox">
+			<view class="tui-search-input">
+				<!-- #ifdef APP-PLUS || MP -->
+				<icon type="search" :size='13' color='#333'></icon>
+				<!-- #endif -->
+				<!-- #ifdef H5 -->
+				<view>
+					<com-icons type="search" :size='16' color='#333333'></com-icons>
+				</view>
+				<!-- #endif -->
+				<input confirm-type="search" placeholder="请输入商品名" :focus="true" auto-focus placeholder-class="tui-input-plholder"
+				 class="tui-input" v-model.trim="key" @confirm='search'/>
+				<!-- #ifdef APP-PLUS || MP -->
+				<icon type="clear" :size='13' color='#bcbcbc' @tap="cleanKey" v-show="key"></icon>
+				<!-- #endif -->
+				<!-- #ifdef H5 -->
+				<view @tap="cleanKey" v-show="key"><tui-icon name="close-fill" :size='16' color='#bcbcbc'></tui-icon></view>
+				<!-- #endif -->
+			</view>
+			<view class="tui-cancle" @tap="search">搜索</view>
+		</view>
 	<!-- 	<view class="table-list">
 			<text :class="selectINdex==index?'actives':''" v-for="(item,index) in tabs" :key='index' @click="selectINdexCheck(index)">
 				{{item.name}}
@@ -92,15 +113,20 @@
 </template>
 
 <script>
+	import tuiIcon from "@/components/icon/icon";
+	import tuiTag from "@/components/tag/tag";
 	import tuiButton from "@/components/extend/button/button"
 	import tuiListCell from "@/components/list-cell/list-cell"
 	export default {
 		components: {
 			tuiButton,
 			tuiListCell,
+			tuiIcon,
+			tuiTag
 		},
 		data() {
 			return {
+				key: "",
 				img_url: this.$api.img_url,
 				url:this.$api.img_url,
 				textColor:'#bc0100',
@@ -187,6 +213,12 @@
 			}
 		},
 		methods: {
+			cleanKey: function() { //清空搜索
+				this.key = ''
+			},
+			search(){
+				
+			},
 			selectINdexCheck(index){ //点击切换
 				this.selectINdex=index
 				this.form={
@@ -444,7 +476,7 @@
 	}
 
 	.tui-order-list {
-		margin-top: 30rpx;
+		margin-top: 120rpx;
 		margin-bottom: 80rpx;
 	}
 
@@ -605,5 +637,85 @@
 	.btn-gary{
 		color: #8F8D8E !important;
 		border: 1px solid #8F8D8E !important;
+	}
+	
+	.tui-searchbox {
+		width: 100%;
+		padding: 30rpx;
+		background: #fff;
+		box-sizing: border-box;
+		display: flex;
+		align-items: center;
+		position: fixed;
+		/* #ifdef H5 */
+		top: 88rpx;
+		/* #endif */
+		/* #ifdef  MP  */
+		top: 0rpx;
+		/* #endif */
+		left: 0;
+		z-index: 9999;
+	}
+	
+	.tui-search-input {
+		width: 90%;
+		height: 66rpx;
+		border-radius: 35rpx;
+		padding: 0 30rpx;
+		box-sizing: border-box;
+		background: #f2f2f2;
+		display: flex;
+		align-items: center;
+		flex-wrap: nowrap;
+	}
+	
+	.tui-input {
+		flex: 1;
+		color: #333;
+		padding: 0 16rpx;
+		font-size: 11pt;
+	}
+	
+	.tui-input-plholder {
+		font-size: 11pt;
+		color: #b2b2b2;
+	}
+	
+	.tui-cancle {
+		color: #888;
+		font-size: 11pt;
+		padding-left: 30rpx;
+		flex-shrink: 0;
+	}
+	
+	.tui-history-header {
+		display: flex;
+		align-items: center;
+		justify-content: space-between;
+		padding: 30rpx 0;
+	}
+	
+	.tui-icon-delete {
+		padding: 10rpx;
+	}
+	
+	.tui-search-title {
+		font-size: 11pt;
+		font-weight: bold;
+	}
+	
+	.tui-hot-header {
+		padding: 30rpx 0;
+	}
+	
+	.tui-tag-class {
+		display: inline-block;
+		margin-bottom: 20rpx;
+		margin-right: 20rpx;
+		font-size: 9pt !important;
+	}
+	.tui-history-content{
+		display: flex;
+		flex-wrap: wrap;
 	}
 </style>
