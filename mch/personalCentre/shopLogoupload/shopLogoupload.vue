743585abc17da1f341da74f3970be65b59f22dc4
zx-1992 <1447966060@qq.com>
2021-11-22 11:25:21 +0800

    11.22

--- mch/personalCentre/shopLogoupload/shopLogoupload.vue
+++ mch/personalCentre/shopLogoupload/shopLogoupload.vue
@@ -9,6 +9,10 @@
 				
 			</view>
 		</view>
+		<view class="btn" style="position: fixed;left: 0;bottom: 0;width: 100%;overflow: hidden;
+		height: 92rpx;background:#FF7104 ;text-align: center;line-height: 92rpx;font-size: 36rpx;color: #fff;" @click="uploadimgList">
+			确定上传
+		</view>
 	</view>
 </template>
 
@@ -18,6 +22,7 @@
 			return {
 				imgList:[],
 				img_url: this.$api.img_url,
+				file:'',
 			};
 		},
 		onLoad() {
@@ -37,85 +42,103 @@
 				uni.chooseImage({
 					sourceType:['album','camera'], 
 					success: function(res) {
-						var file = res.tempFilePaths
-						console.log(file)
-						for (let i = 0; i < file.length; i++) {
-						    var requestData = {
-						    	serverUrl: that.$api.default.upload+'&width=6000&height=6000&type=1',
-						    	fileKeyName: "file",
-						    	file: file[i]
-						    }
-							that.$http.uploadFile(requestData).then(function(res) {
-								if(res.code==0){
-									var url = res.data.url
-									that.$http
-										.request({
-											url: that.$api.moreShop.setShopLogo,
-											method: 'POST',
-											showLoading: true,
-											data:{
-												act:'add',
-												pic_url:url,
-											}
-										})
-										.then(result => {
-											if(result.code==0){
-												console.log(result.data.pic_urls)
-												let List=result.data.pic_urls
-												let logoList=[]
-												List.forEach((item)=>{
-													if(typeof(item)!='object'){
-														logoList.push(item)
-													}
-												})										
-												uni.setStorageSync('imglist',logoList)
-												that.imgList=logoList
-											}else{
-												that.$http.toast(result.msg);
-											}
-										});
-								}else{
-									uni.showToast({
-										title: '图片太大，请重新上传',
-										icon: 'none'
-									});
-									setTimeout(function() {
-										uni.hideToast();
-									}, 2000);
-								}
-							})						
-						}
+						that.file = res.tempFilePaths
+						let logoList=[]
+						that.file.forEach((item)=>{
+							if(typeof(item)!='object'){
+								logoList.push(item)
+							}
+						})										
+						that.imgList=logoList
 					}
 				})
 			},
+			uploadimgList(){ //确认上传
+				let that=this
+				for (let i = 0; i < that.file.length; i++) {
+				    var requestData = {
+				    	serverUrl: that.$api.default.upload+'&width=6000&height=6000&type=1',
+				    	fileKeyName: "file",
+				    	file: that.file[i]
+				    }
+					that.$http.uploadFile(requestData).then(function(res) {
+						if(res.code==0){
+							var url = res.data.url
+							that.$http
+								.request({
+									url: that.$api.moreShop.setShopLogo,
+									method: 'POST',
+									showLoading: true,
+									data:{
+										act:'add',
+										pic_url:url,
+									}
+								})
+								.then(result => {
+									if(result.code==0){
+										let List=result.data.pic_urls
+										let logoList=[]
+										List.forEach((item)=>{
+											if(typeof(item)!='object'){
+												logoList.push(item)
+											}
+										})										
+										uni.setStorageSync('imglist',logoList)
+										that.imgList=logoList
+									}else{
+										that.$http.toast(result.msg);
+									}
+								});
+						}else{
+							uni.showToast({
+								title: '图片太大，请重新上传',
+								icon: 'none'
+							});
+							setTimeout(function() {
+								uni.hideToast();
+							}, 2000);
+						}
+					})						
+				}
+			},
 			deleted(item){
 				let that=this
-				that.$http
-					.request({
-						url: that.$api.moreShop.setShopLogo,
-						method: 'POST',
-						showLoading: true,
-						data:{
-							act:'sub',
-							pic_url:item,
+				if(uni.getStorageSync('imglist')&&uni.getStorageSync('imglist').length>0){
+					that.$http
+						.request({
+							url: that.$api.moreShop.setShopLogo,
+							method: 'POST',
+							showLoading: true,
+							data:{
+								act:'sub',
+								pic_url:item,
+							}
+						})
+						.then(result => {
+							if(result.code==0){
+								that.$http.toast('删除成功');
+								let List=result.data.pic_urls
+								let logoList=[]
+								List.forEach((item)=>{
+									if(typeof(item)!='object'){
+										logoList.push(item)
+									}
+								})										
+								uni.setStorageSync('imglist',logoList)
+								that.imgList=logoList
+							}else{
+								that.$http.toast(result.msg);
+							}
+						});
+				}else{
+					let logoList=[]
+					for(let i=0;i<that.imgList.length;i++){
+						if(that.imgList[i]!=item){
+							logoList.push(that.imgList[i])
 						}
-					})
-					.then(result => {
-						if(result.code==0){
-							that.$http.toast('删除成功');
-							let List=result.data.pic_urls
-							let logoList=[]
-							List.forEach((item)=>{
-								if(typeof(item)!='object'){
-									logoList.push(item)
-								}
-							})										
-							uni.setStorageSync('imglist',logoList)
-							that.imgList=logoList
-						}else{
-							that.$http.toast(result.msg);
-						}
-					});
+					}
+					that.imgList=logoList
+				}
 			},
 		}
 	}
