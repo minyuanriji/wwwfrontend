8ded942ec08f663285c5fffcc9eac601ab2b6d98
zx-1992 <1447966060@qq.com>
2021-11-24 14:38:05 +0800

    11.24

--- mch/personalCentre/waiteDetail/waiteDetail.vue
+++ mch/personalCentre/waiteDetail/waiteDetail.vue
@@ -14,6 +14,10 @@
 				</view>
 			</view>
 		</view>
+		<!--加载loadding-->
+		<main-loadmore :visible="loadding" :index="3" type="red"></main-loadmore>
+		<main-nomore :visible="!pullUpOn" bgcolor="#FFFFFF"></main-nomore>
+		<!--加载loadding-->
 	</view>
 </template>
 
@@ -28,7 +32,9 @@
 					status:'unconfirmed',//状态：canceled已取消，success已结算，unconfirmed待结算
 				},
 				list:[],
-				page_count:''
+				page_count:'',
+				pullUpOn:true,
+				loadding:false,
 			};
 		},
 		onLoad() {
@@ -41,6 +47,8 @@
 					status:'unconfirmed',//状态：canceled已取消，success已结算，unconfirmed待结算
 				},
 				this.selectIndex=index
+				this.pullUpOn=true
+				this.loadding=false
 				if(index==0){this.form.status='unconfirmed'}
 				if(index==1){this.form.status='success'}
 				if(index==2){this.form.status='canceled'}
@@ -54,14 +62,18 @@
 						url: this.$api.moreShop.getcountList,
 						method: 'POST',
 						data:this.form,
-						showLoading: true,
 					})
 					.then(res => {
 						if(res.code==0){
-							if(res.data.list.length==0)return false
-							let arr= res.data.list;
-							this.list=this.list.concat(arr)
-							this.page_count= res.data.pagination.page_count;
+							if(res.data.list.length==0){
+								this.pullUpOn=false
+								return false
+							}
+							let list= res.data.list;
+							var arr=this.list.concat(list)
+							this.list =arr
+							this.page_count = res.data.pagination.page_count;
+							this.pullUpOn=true
 						}else{
 							this.$http.toast(res.msg);
 						}
@@ -69,7 +81,11 @@
 			}
 		},
 		onReachBottom() {
+			this.pullUpOn=true
+			this.loadding=true
 			if(this.form.page==this.page_count){
+				this.pullUpOn=false
+				this.loadding=false
 				return false;
 			}
 			this.form.page=this.form.page+1
@@ -84,7 +100,7 @@
 	.select-type text{width:25% ;display: block;height: 70rpx;text-align: center;line-height: 70rpx;}
 	.active{background: #FF7104;color: #fff;}
 	.watitecount-list{width: 100%;overflow: hidden;}
-	.item{width: 100%;overflow: hidden;display: flex;justify-content: space-between;margin: 20rpx 0;border-bottom: 1rpx solid #C4C4C4;}
+	.item{width: 100%;overflow: hidden;display: flex;justify-content: space-between;margin: 30rpx 0;border-bottom: 1rpx solid #C4C4C4;}
 	.item-left{width: 60%;font-size: 26rpx;margin-bottom: 10rpx;}
 	.item-right{width: 35%;font-size: 28rpx;display: flex;flex-wrap: wrap;margin-bottom: 10rpx;}
 	.item-right text{display: block;width: 100%;text-align: center;}
