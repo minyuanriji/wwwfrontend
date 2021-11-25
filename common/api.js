a908b3e20a86987bfc7cfa5644f4ec4890b97d3f
zx-1992 <1447966060@qq.com>
2021-11-25 18:00:36 +0800

    11.25

--- common/api.js
+++ common/api.js
@@ -6,15 +6,15 @@
 
 
 // 测试服务器
-//let api_root = 'https://dev.mingyuanriji.cn/web/index.php?r=';
-//let url = 'https://dev.mingyuanriji.cn/web/static/'; //图片路径
-//let plugins_img_url = 'https://dev.mingyuanriji.cn/web/plugins/'; //扩展页面图片路径
+let api_root = 'https://dev.mingyuanriji.cn/web/index.php?r=';
+let url = 'https://dev.mingyuanriji.cn/web/static/'; //图片路径
+let plugins_img_url = 'https://dev.mingyuanriji.cn/web/plugins/'; //扩展页面图片路径
 
 
 // 本地服务器
-let api_root = 'http://local.mingyuanriji.cn/web/index.php?r=';
-let url = 'http://local.mingyuanriji.cn/web/static/'; //图片路径
-let plugins_img_url = 'http://local.mingyuanriji.cn/web/plugins/'; //扩展页面图片路径 
+// let api_root = 'http://local.mingyuanriji.cn/web/index.php?r=';
+// let url = 'http://local.mingyuanriji.cn/web/static/'; //图片路径
+// let plugins_img_url = 'http://local.mingyuanriji.cn/web/plugins/'; //扩展页面图片路径 
 
 
 //wx493ce82aab45e4da
